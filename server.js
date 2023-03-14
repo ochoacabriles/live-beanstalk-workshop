8cd23770ae59b9bad2ffa19e7d340df64d4578d3
Rafael <ochoacabriles@gmail.com>
2023-03-14 17:09:09 -0600

    ✨ Express basic configuration

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/api', (_, res) => {
+  res.json({ status: 'working' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server ready on port ${process.env.PORT}`);
+});
